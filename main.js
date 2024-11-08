6aa6280972cc6131113df092f3e1b0174cda3eca
Alina Bregvadze <alina.bregvadze@gmail.com>
2024-11-08 22:04:09 +0400

    variables

---
+++ main.js
@@ -0,0 +1,80 @@
+console.log("main.js");
+console.log("hello world");
+
+var lessonNumber = 15;
+
+// console.log(lessonNumber);
+
+let producQuantity = 1;
+const productName = "Monitor";
+
+productPrice = 100;
+
+// console.log("producQuantity", producQuantity);
+// console.log("productName", productName);
+
+producQuantity = 2;
+// console.log("producQuantity", producQuantity);
+
+let courseName = "front-end'quote'";
+let courseName2 = 'front-end  "quote" ';
+let courseName3 = `front-end  " '`;
+// console.log(courseName, courseName2, courseName3);
+
+const studentsNumber = 20;
+const lectureDuration = 1.5;
+// console.log(studentsNumber);
+// console.log(typeof studentsNumber);
+
+const isOnline = true;
+const isOffline = false;
+
+// console.log(isOnline, isOffline);
+// console.log(typeof isOffline);
+
+let numberofStudentsWhoFinishedCourse2; // undefined
+let numberofStudentsWhoFinishedCourse3 = undefined; // undefined
+
+// console.log(numberofStudentsWhoFinishedCourse2);
+// console.log(typeof numberofStudentsWhoFinishedCourse2);
+
+const studentsWhoFinishedCourse = null;
+
+/*
+console.log(studentsWhoFinishedCourse);
+console.log(typeof studentsWhoFinishedCourse); //object
+*/
+
+const price1 = 10;
+const price2 = 20;
+let discount = 2;
+
+const sum = price1 + price2;
+// console.log("addition", sum);
+const deliveryPrice = 5;
+
+const finalPrice = sum - discount + deliveryPrice;
+// console.log("subsctraction", finalPrice);
+
+// user entered promo code
+discount = 5;
+
+const productQ = 5;
+const productTotalPrice = productQ * finalPrice;
+// console.log("multiplication", productTotalPrice);
+
+const productTotalPrice2 = 100;
+const prodQ2 = 10;
+const prodPricePerUnit = productTotalPrice2 / prodQ2;
+// console.log("division", prodPricePerUnit);
+
+const remainder = 10 % 3; // 1
+const power = 2 ** 3; // 8
+
+// console.log(16 + 4 + "text");
+// console.log(5 + "5");
+// console.log(5 - "text"); //NaN
+
+const showText = "total price is: " + productTotalPrice + " USD";
+const showText2 = `total price is: ${productTotalPrice} USD`;
+// console.log(showText);
